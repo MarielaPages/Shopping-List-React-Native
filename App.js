7696150c9bb4e7191d45bfdc223d3a1ce84d8ec4
Mariela Pages <mai.pages5@gmail.com>
2023-04-28 00:18:34 +0200

    modificaicones

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputContainer}>
+        <TextInput placeholder='Enter something' />
+        <Button title='Add' />
+      </View>
     </View>
   );
 }
@@ -14,7 +15,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  inputContainer: {
+    marginTop: 40,
+    marginHorizontal: 20
+  }
 });
