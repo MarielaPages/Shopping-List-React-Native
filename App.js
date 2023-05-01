a92fd8d731ef76695841913b05d8e22708e63f56
Mariela Pages <mai.pages5@gmail.com>
2023-05-01 17:14:27 +0200

    style changes

--- App.js
+++ App.js
@@ -1,23 +1,14 @@
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { Text, View, TextInput, Button } from 'react-native';
+import { styles } from './styles.js'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.inputContainer}>
-        <TextInput placeholder='Enter something' />
-        <Button title='Add' />
+        <TextInput placeholder='Enter something' style={styles.input}/>
+        <Button title='Add' color={'#C28CAE'}/>
       </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  inputContainer: {
-    marginTop: 40,
-    marginHorizontal: 20
-  }
-});
---
+++ styles.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      marginHorizontal: 20 //lo agrego para que el contenedor general de por si tenga 10 de margen ademas de lo que se ordene antentro con el space-around
+    },
+    inputContainer: {
+      flexDirection: 'row',
+      width: '100%',
+      marginTop: 40,
+      // marginHorizontal: 20,
+      alignItems: 'center',
+      justifyContent: 'space-between' //asi queda tmb espaciado afuera al mismo nivel de distancia entre ellos
+    },
+    input: {
+      width: '80%',
+      borderBottomWidth: 0.5,
+      borderBottomColor: '#BA7BA1',
+      // marginRight:20
+    }
+  });
+  
